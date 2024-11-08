652251c9814a7db763298094cfb77f3cdee44567
CodeWithAnum  🇵🇰 <anumhussain875@gmail.com>
2024-11-08 20:12:51 +0500

    Add files via upload

---
+++ Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Header () {
+    return(
+        <div className="h-[134] p-4"style={{backgroundColor:"rgb(162, 152, 117)"}}>
+            <h1 className="text-3xl font-bold text-white">MANZZARI</h1>
+        </div>
+    )
+}
+
+export default Header
